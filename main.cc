ea87fa70a92c734f0fe22e128a05e4776c653543
Michael Chen <michael.jchen04@gmail.com>
2017-01-24 15:48:17 -0800

    Parse for port number

--- main.cc
+++ main.cc
@@ -1,4 +1,6 @@
 #include "config_parser.h"
+#include <stdlib.h>
+using namespace std;
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
@@ -10,6 +12,11 @@ int main(int argc, char* argv[]) {
   NginxConfig config;
   config_parser.Parse(argv[1], &config);
 
-  printf("%s", config.ToString().c_str());
+  int config_size = (unsigned long) config.statements_[0]->child_block_->statements_.size();
+  for (int i = 0; i < config_size; i++) {
+  	if (config.statements_[0]->child_block_->statements_[i]->tokens_[0] == "listen") {
+  		cout << stoi(config.statements_[0]->child_block_->statements_[i]->tokens_[1]) << endl;
+  	}
+  }
   return 0;
 }
