fd65586ac75c76a4821533bf4a017f532eccc66d
Spencer Gervais <spencergervais21@gmail.com>
2017-01-25 00:09:39 +0000

    Added echo server code, session, server objects. Added simple Makefile. HTTP not yet supported.

--- main.cc
+++ main.cc
@@ -1,5 +1,28 @@
-#include "config_parser.h"
+//  #include "config_parser.h"
+#include "session.h"
+#include "server.h"
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <boost/asio.hpp>
 
 int main(int argc, char* argv[]) {
-  return 0;
+    try
+    {
+        if (argc != 2)
+        {
+            std::cerr << "Usage: echo_server <port>\n";
+            return 1;
+        }
+        
+        boost::asio::io_service io_service;
+        Server s(io_service, std::atoi(argv[1]));
+        io_service.run();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+
+    return 0;
 }
