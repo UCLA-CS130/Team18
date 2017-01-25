b92aafa3338eb9a466e4f2dcef9795eff4595e1b
Michael Chen <michael.jchen04@gmail.com>
2017-01-24 16:35:41 -0800

    Remove build script

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
-echo_server:
+webserver:
 	g++ -o webserver server.cc session.cc config_parser.cc main.cc -std=c++11 -Wall -Werror -lboost_system
--- build_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-case `uname` in
-  Linux) g++ server.cc session.cc config_parser.cc main.cc -std=c++0x -g -Wall -Werror -lboost_system -o webserver;;
-  Darwin) clang++ config_parser.cc main.cc -std=c++11 -g -Wall -stdlib=libc++ -o webserver;;
-  *) echo "Unknown operating system";;
-esac
