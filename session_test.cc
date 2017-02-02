312ef53b0f1c54662191329c4e3802b60ff6b2df
mjchen04 <michael.jchen04@gmail.com>
2017-02-01 19:01:44 -0800

    Modify session_test

--- session_test.cc
+++ session_test.cc
@@ -8,38 +8,45 @@ class SessionInputTest : public ::testing::Test {
   protected:
   
     //Tests Parse with config in stringstream form
-    bool CheckString(std::string str) {
-      boost::asio::io_service io_service;
-      tcp::socket socket(io_service);
-      Session s(&socket);
-      char buffer[256];
-      std::size_t size = str.size();
-      strncpy(buffer, str.c_str(), size);
-      return s.check_input(size, buffer);
-    }
+  bool CheckString(std::string str) {
+    boost::asio::io_service io_service;
+    tcp::socket socket(io_service);
+    Session s(&socket);
+    char buffer[256];
+    std::size_t size = str.size();
+    strncpy(buffer, str.c_str(), size);
+    return s.check_input(size, buffer);
+  }
 
-     std::string GetOutput(std::string str) {
-      boost::asio::io_service io_service;
-      tcp::socket socket(io_service);
-      Session s(&socket);
-      std::size_t length = s.prepare_response(200, str);
-      return s.output_as_string(length);
-    }
-    
+  std::string GetOutput(std::string str, int status_code) {
+    boost::asio::io_service io_service;
+    tcp::socket socket(io_service);
+    Session s(&socket);
+    std::size_t length = s.prepare_response(status_code, str);
+    return s.output_as_string(length);
+  }
 };
 
 
 TEST_F(SessionInputTest, CompleteMessage) {
-    EXPECT_TRUE(CheckString("host\r\n\r\n"));
+  EXPECT_TRUE(CheckString("host\r\n\r\n"));
 }
 
 TEST_F(SessionInputTest, IncompleteMessage) {
-    EXPECT_FALSE(CheckString("host"));
+  EXPECT_FALSE(CheckString("host"));
 }
 
 TEST_F(SessionInputTest, SampleOutput) {
-    std::string message = "abc";
-    std::string output = GetOutput(message);
-    EXPECT_TRUE(output.find(message) != std::string::npos);
-    EXPECT_GT(output.size(), message.size());
+  std::string message = "abc";
+  std::string output = GetOutput(message, 200);
+  EXPECT_TRUE(output.find(message) != std::string::npos);
+  EXPECT_GT(output.size(), message.size());
+}
+
+TEST_F(SessionInputTest, BadRequest) {
+  std::string message = "abc";
+  std::string output = GetOutput(message, 400);
+
+  EXPECT_TRUE(output.find(message) != std::string::npos);
+  EXPECT_GT(output.size(), message.size());
 }
