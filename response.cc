f8413fb3ca6838781fa6c04c85dbf0e0f3e091a2
mjchen04 <michael.jchen04@gmail.com>
2017-02-05 23:25:45 -0800

    Modify to_buffer, add status_type enum, to_string method

--- response.cc
+++ response.cc
@@ -2,29 +2,45 @@
 #include <stdio.h>
 #include <string.h>
 
-std::size_t response::to_buffer(char* response_buf) {
-  std::string response_msg;
-  response_msg += get_header("Status-Line");
-  response_msg += get_header("Content-Type");
-  response_msg += get_header("Content-Length");
+namespace status_strings {
+  const std::string ok = "200 OK\r\n";
+  const std::string bad_request = "400 Bad Request\r\n";
+  const std::string not_found = "404 Not Found\r\n";
+}
+
+std::string response::to_buffer() {
+  std::string response_msg = "HTTP/" + http_version + " " + to_string(status);
+
+  std::map<std::string, std::string>::iterator it;
+  for (it = headers.begin(); it != headers.end(); it++) {
+    response_msg += get_header(it->first);
+  }
   response_msg += "\r\n";
   response_msg += body;
 
-  strncpy(response_buf, response_msg.c_str(), length);
-  return length;
+  return response_msg;
 }
 
 std::string response::get_header(std::string header) {
   std::string formatted_header = "";
   std::string header_value = headers[header];
 
-  if (header == "Status-Line") {
-  	formatted_header = header_value + "\r\n"; 
-  } else if (header == "Content-Type") {
+  if (header == "Content-Type") {
   	formatted_header = "Content-Type: " + header_value + "\r\n";
   } else if (header == "Content-Length") {
  	formatted_header = "Content-Length: " + header_value + "\r\n";
   }
 
   return formatted_header;
+}
+
+std::string to_string(response::status_type status) {
+  switch (status) {
+    case response::ok:
+      return status_strings::ok;
+    case response::bad_request:
+      return status_strings::bad_request;
+    case response::not_found:
+      return status_strings::not_found;
+  }
 }
--- response.h
+++ response.h
@@ -6,12 +6,19 @@
 
 class response {
   public:
-    std::size_t to_buffer(char* response_buf);
+    std::string to_buffer();
+    std::string http_version;
+    enum status_type {
+  	  ok = 200,
+  	  bad_request = 400,
+  	  not_found = 404
+    } status;
     std::map<std::string, std::string> headers;
     std::string body;
     std::size_t length;
   private:
   	std::string get_header(std::string header);
+  	std::string to_string(response::status_type status);
 };
 
 #endif
