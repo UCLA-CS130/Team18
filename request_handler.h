a6d8b5c75c14f6e9413a4328e3faee5c48927f30
mjchen04 <michael.jchen04@gmail.com>
2017-02-05 23:35:53 -0800

    Refacter Request/Response name. Remove Const

--- request_handler.h
+++ request_handler.h
@@ -1,12 +1,12 @@
 #ifndef REQUEST_HANDLER
 #define REQUEST_HANDLER
 
-class request;
-class response;
+class Request;
+class Response;
 
 class request_handler {
   public:
-    virtual void handle_request(const request& req, response& rep) = 0;
+    virtual void handle_request(Request& req, Response& rep) = 0;
 };
 
 #endif
