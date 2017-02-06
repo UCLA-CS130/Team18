5bc8af6ab6c3113f8b8088ba83f4781eaad6e598
mjchen04 <michael.jchen04@gmail.com>
2017-02-05 22:22:10 -0800

    Try again to fix weird indentation

--- response.h
+++ response.h
@@ -6,7 +6,7 @@
 
 class response {
   public:
- 	  std::size_t to_buffer(char* response_buf);
+    std::size_t to_buffer(char* response_buf);
     std::map<std::string, std::string> headers;
     std::string body;
     std::size_t length;
