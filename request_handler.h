48deabb15b9a5145828873fe60739c432eb49068
mjchen04 <michael.jchen04@gmail.com>
2017-02-06 11:24:46 -0800

    Change handle_request parameters to pointers

--- request_handler.h
+++ request_handler.h
@@ -6,7 +6,7 @@ class Response;
 
 class request_handler {
   public:
-    virtual void handle_request(Request& req, Response& rep) = 0;
+    virtual void handle_request(Request* req, Response* rep) = 0;
 };
 
 #endif
