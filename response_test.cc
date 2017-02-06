5751891faefa86f0f4b8db8b5c7e4f4bc53ddc77
mjchen04 <michael.jchen04@gmail.com>
2017-02-06 14:52:48 -0800

    Add more response unit tests

--- response_test.cc
+++ response_test.cc
@@ -18,13 +18,37 @@ class ResponseTest : public ::testing::Test {
     Response* _response;	
 };
 
-TEST_F(ResponseTest, SimpleResponse) {
+TEST_F(ResponseTest, ok_Response) {
   std::map<std::string, std::string> headers;
   headers["Content-Type"] = "text/plain";
   headers["Content-Length"] = "10";
   EXPECT_EQ("HTTP/1.0 200 OK\r\nContent-Length: 10\r\nContent-Type: text/plain\r\n\r\nVery simple body",
   			GetBuffer("HTTP/1.0",
 					  Response::ok,
+					  headers,
+					  "Very simple body")
+  		   ) << "Expected different buffer";
+}
+
+TEST_F(ResponseTest, bad_request_Response) {
+  std::map<std::string, std::string> headers;
+  headers["Content-Type"] = "text/plain";
+  headers["Content-Length"] = "10";
+  EXPECT_EQ("HTTP/1.0 400 Bad Request\r\nContent-Length: 10\r\nContent-Type: text/plain\r\n\r\nVery simple body",
+  			GetBuffer("HTTP/1.0",
+					  Response::bad_request,
+					  headers,
+					  "Very simple body")
+  		   ) << "Expected different buffer";
+}
+
+TEST_F(ResponseTest, not_found_Response) {
+  std::map<std::string, std::string> headers;
+  headers["Content-Type"] = "text/plain";
+  headers["Content-Length"] = "10";
+  EXPECT_EQ("HTTP/1.0 404 Not Found\r\nContent-Length: 10\r\nContent-Type: text/plain\r\n\r\nVery simple body",
+  			GetBuffer("HTTP/1.0",
+					  Response::not_found,
 					  headers,
 					  "Very simple body")
   		   ) << "Expected different buffer";
