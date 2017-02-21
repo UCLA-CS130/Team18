1082a0fa8dd291b464ee8ebaf3709949b603a0e0
Spencer Gervais <spencergervais21@gmail.com>
2017-02-21 20:07:47 +0000

    Multithreading

--- server.h
+++ server.h
@@ -11,6 +11,10 @@
 #include <cstdlib>
 #include <boost/asio.hpp>
 #include "config_options.h"
+#include "boost/thread.hpp"
+#include <vector>
+#include <boost/shared_ptr.hpp>
+#include "session.h"
 
 using boost::asio::ip::tcp;
 
@@ -27,9 +31,13 @@ private:
   NginxConfig* config_;
   config_options* options_;
   void do_accept();
+  void handle_stop();
+  //void spawn_thread(tcp::socket* sock, NginxConfig* config);
   tcp::acceptor acceptor_;
   tcp::socket socket_;
   short port_num;
+  boost::shared_ptr<Session> new_connection_;
+//  boost::asio::signal_set signals_;
 };
 
 #endif
