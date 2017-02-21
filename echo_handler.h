e9b77452c3f68db9e1af7c82beda59cafb60170a
mjchen04 <michael.jchen04@gmail.com>
2017-02-20 20:24:54 -0800

    Fix Poor indentation of echo_handler.h

--- echo_handler.h
+++ echo_handler.h
@@ -5,10 +5,10 @@
 
 class EchoHandler : public RequestHandler {
   public:
-  	virtual RequestHandler::Status Init(const std::string& uri_prefix,
-										const NginxConfig& config);
+    virtual RequestHandler::Status Init(const std::string& uri_prefix,
+					const NginxConfig& config);
     virtual RequestHandler::Status HandleRequest(const Request& request,
-    							 				 Response* repsponse);
+						 Response* repsponse);
 };
 
 REGISTER_REQUEST_HANDLER(EchoHandler);
