1d0b647977d8f2dd9ae589a9255e3ef6fe9c037d
mjchen04 <michael.jchen04@gmail.com>
2017-02-20 20:21:39 -0800

    Fix Poor indentation of bad_request_handler.cc

--- bad_request_handler.cc
+++ bad_request_handler.cc
@@ -1,13 +1,14 @@
 #include "bad_request_handler.h"
 
 RequestHandler::Status BadRequestHandler::Init(const std::string& uri_prefix,
-                           	   				   const NginxConfig& config)
+                                               const NginxConfig& config)
 {
   return RequestHandler::Status::OK;
 }
 
 RequestHandler::Status BadRequestHandler::HandleRequest(const Request& request,
-    							 				 		 Response* response) {
+                                                        Response* response)
+{
   response->SetStatus(Response::bad_request);
   response->AddHeader("Content-Length", "0");
   return RequestHandler::Status::OK;
