59e6ea4d04ef55be1e54eecafda1aae20c767459
Spencer Gervais <spencergervais21@gmail.com>
2017-03-07 22:15:32 +0000

    Added tools to setup docker image and run on EC2 instance

---
+++ aws_deploy.sh
@@ -0,0 +1,7 @@
+docker save -o httpserver.tar httpserver
+scp -i key1.pem httpserver.tar ubuntu@ec2-35-166-129-63.us-west-2.compute.amazonaws.com:~/httpserver.tar
+ssh -i key1.pem ubuntu@ec2-35-166-129-63.us-west-2.compute.amazonaws.com << ENDSSH 
+docker load -i httpserver.tar
+docker run --rm -t -p 8080:8080 httpserver
+ENDSSH
+
